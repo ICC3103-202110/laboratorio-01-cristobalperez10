2b287714e645b992effed95987d534cc36d41e6e
cristobalperez10 <caperez10@miuandes.cl>
2021-03-26 17:09:22 -0300

    final final

--- lab_1_poo_final.py
+++ lab_1_poo_final.py
@@ -23,7 +23,7 @@ table.append(mat)
 table.append(mat2)
 hided_cards = np.matrix(table)
 
-def ast_mat():                                  #funcion matriz de *s
+def ast_mat():                                  #Funcion que me genera la matriz de asteriscos
     ma = table.copy()
     for i in range(2):
         for j in range(n_cards):
@@ -32,9 +32,9 @@ def ast_mat():                                  #funcion matriz de *s
 mat_ast = np.matrix(ast_mat())                  #Esta es la matriz que se muestra
 print(hided_cards)                              #Matriz con la que trabajo
 
-def end_game():
-    global points_p1, points_p2
-    total_points = points_p1 + points_p2 
+def end_game():                                         #Funcion para determinar el final del juego
+    global points_p1, points_p2                         #cuando no queden cartas en el tablero
+    total_points = points_p1 + points_p2    
     if total_points == n_cards:
         if points_p1 > points_p2:
             print("Player 1 won!")
@@ -43,7 +43,7 @@ def end_game():
         else:
             print("Draw!")
         return 0            
-def sum_points(turn,card1,card2):
+def sum_points(turn,card1,card2):                        #Funcion que va a sumar puntos al que gane
     if card1 == card2:
         print("You won! It is still your turn...")
         if turn == 1:
@@ -71,9 +71,9 @@ points_p1 = 0
 points_p2 = 0
 turn = 1
 
-while True:
+while True:                                                                                #Turno del jugador 1, ingresa las coordenadas de las cartas que quiere voiltear
     if turn == 1:
-        coord1 = input("Player 1 enter your coordinates in format x,y: ").split(",")
+        coord1 = input("Player 1 enter your coordinates in format x,y: ").split(",")        
         x1,y1 = int(coord1[0]),int(coord1[1])
         print("your chosed card is: ", hided_cards[x1,y1])
         c1 = hided_cards[x1,y1]
@@ -87,7 +87,7 @@ while True:
         showed_mat[x2,y2] = c2
         print(showed_mat)
         sum_points(turn, c1, c2)
-    elif turn == 2:
+    elif turn == 2:                                                                         #Turno del jugador 2, ingresa las coordenadas de las cartas que quiere voiltear
         coord3 = input("Player 2 enter your coordinates in format x,y: ").split(",")
         x3,y3 = int(coord3[0]),int(coord3[1])
         print("your chosed card is: ", hided_cards[x3,y3])
